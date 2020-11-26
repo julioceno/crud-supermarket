d5584909b15c7090eebc8314ec64d79b590e27de
Julio Nepomuceno <julioreplay@gmail.com>
2020-11-26 01:09:56 -0300

    finalizando menu hamburguer

--- javascript/page-landing.js
+++ javascript/page-landing.js
@@ -1,15 +1,33 @@
+const checkboxMenuHamburguer = document.getElementById('menu-hamburguer')
 const header = document.getElementById('header')
 const appearMenu = document.getElementById('appear-menu')
-const checkboxMenuHamburguer = document.getElementById('menu-hamburguer')
+const search = document.getElementById("search")
 
 function menuHamburguer() {
 
     if (checkboxMenuHamburguer.checked) {
         header.style.background = "var(--color-cyan)"
         appearMenu.style.display = "block"
+        search.style.display ="none"
     } else {
         header.style.background = "var(--color-blue)"
         appearMenu.style.display = "none"
+        search.style.display ="flex"
+
+    }
+
+
+    let windowWidth = window.innerWidth;
+    if (windowWidth > 1100) {
+        header.style.background = "var(--color-blue)"
+        appearMenu.style.display = "none"
+        search.style.display ="flex"
     }
 }
 
+
+
+
+  window.addEventListener('resize', function(){
+      menuHamburguer();
+  });
