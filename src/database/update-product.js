13130fb477ad57929dce1b8ed60fe56fe7c275bc
Julio Nepomuceno <julioreplay@gmail.com>
2020-11-30 12:39:49 -0300

    adicionando o R$ quando um valor de um produto é atualizado

--- src/database/update-product.js
+++ src/database/update-product.js
@@ -4,7 +4,7 @@ async function updateProduct(db, id, newProduct) {
     await db.all(`UPDATE products Set name = "${ newProduct.newName || product[0].name}" WHERE id = "${id}" `)
     await db.all(`UPDATE products Set image = "${newProduct.newImage  || product[0].image }" WHERE id = "${id}" `)
     await db.all(`UPDATE products Set brand = "${ newProduct.newBrand || product[0].brand}" WHERE id = "${id}" `)
-    await db.all(`UPDATE products Set price = "${ newProduct.newPrice || product[0].price}" WHERE id = "${id}" `)
+    await db.all(`UPDATE products Set price = "R$${ newProduct.newPrice || product[0].price}" WHERE id = "${id}" `)
     await db.all(`UPDATE products Set amount = "${ newProduct.newAmount || product[0].amount}" WHERE id = "${id}" `)
 } 
 
